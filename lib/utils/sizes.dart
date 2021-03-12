e0a4792976a5aa6dc171925c1515a7515c417c2a
NguyeenVuu <16521756@gm.uit.edu.vn>
2021-03-12 16:45:50 +0700

    add 2

---
+++ lib/utils/sizes.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class DeviceSize {
+  static double _width;
+  static double _height;
+  static Orientation _orientation;
+
+  static void init(BuildContext context) {
+    var screenSize = MediaQuery.of(context);
+    DeviceSize._height = screenSize.size.height;
+    DeviceSize._width = screenSize.size.width;
+    DeviceSize._orientation = screenSize.orientation;
+  }
+
+  static double getWidth() {
+    if (_orientation == Orientation.portrait) {
+      return DeviceSize._width;
+    }
+    return DeviceSize._height;
+  }
+
+  static double getHeight() {
+    if (_orientation == Orientation.portrait) {
+      return DeviceSize._height;
+    }
+    return DeviceSize._width;
+  }
+
+  static Orientation getOrientation() {
+    return DeviceSize._orientation;
+  }
+}
